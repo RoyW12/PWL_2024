6f6518d1cbdce276c68b9f55fa20a616267e16ce
RoyW12 <roywijaya893@gmail.com>
2025-02-19 10:29:10 +0700

    Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+
 
 
 /*
@@ -19,10 +22,19 @@ use App\Http\Controllers\ArticleController;
 |
 */
 
+
+// Route::resource('photos', PhotoController::class);
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+//    ]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+   ]);
+
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
 Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/', [HomeController::class,'index']);
 Route::get('/about', [AboutController::class,'about']);
